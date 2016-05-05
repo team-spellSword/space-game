9367f6b6ec121018a13d378a97f442d66d52860d
MVanderwood <m.b.vanderwood@gmail.com>
2016-05-05 13:23:31 -0500

    Added playerClass factory.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-(function() {
-    angular.module('app', [])
-    .controller('main', ['$scope', function($scope) {
-        $scope.setup = function() {
-            var canvas = document.findElementById('canvas');
-            var ctx = canvas.getContext('2d');
-        };
+var gameApp = angular.module('app', []);
+gameApp.controller('main', ['playerClass', function(playerClass) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    setInterval(draw, 10);
+    var player1 = playerClass.create();
 
-        $scope = window.$scope;
-    }]);
-})();
+    var draw = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        player1.draw(ctx);
+    };
+
+}]);
