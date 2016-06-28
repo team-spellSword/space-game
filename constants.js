a9517a7b1fac215a78ee8a517ce47c296bf0a617
MVanderwood <m.b.vanderwood@gmail.com>
2016-06-27 20:38:41 -0500

    Refactor tileMap constant

--- constants.js
+++ constants.js
@@ -8,29 +8,29 @@ gameApp.constant('environmentConstants', {
     gravityFactor: 0.4
 });
 
-gameApp.constant('tileMap', {
-    cols: 10,
-    rows: 8,
-    tiles: [
-        ['a', '', '', '', '', '', '', '', '', 'a'],
-        ['a', '', '', '', '', '', '', 'b', 'b', 'a'],
-        ['a', '', '', '', '', '', '', '', '', 'a'],
-        ['a', '', 'b', 'b', '', '', '', '', '', 'a'],
-        ['a', '', '', '', '', 'b', 'b', '', '', 'a'],
-        ['a', '', '', '', '', '', '', '', '', 'a'],
-        ['a', '', '', '', '', '', '', '', '', 'a'],
-        ['a', 'a', 'a', 'a', 'c', 'c', 'a', 'a', 'a', 'a']
-    ],
-    keys: {
-        'a': {
-            passibility: 'solid'
-        },
-        'b': {
-            passability: 'one-way'
-        },
-        'c': {
-            passability: 'solid',
-            damaging: true
-        }
+gameApp.constant('tileMap', (function() {
+    function Tile(argsObject) {
+        this.passability = argsObject.passability || 'solid';
+        this.damaging = argsObject.damaging || false;
     }
-});
+
+    return {
+        cols: 10,
+        rows: 8,
+        tiles: [
+            ['a', '', '', '', '', '', '', '', '', 'a'],
+            ['a', '', '', '', '', '', '', 'b', 'b', 'a'],
+            ['a', '', '', '', '', '', '', '', '', 'a'],
+            ['a', '', 'b', 'b', '', '', '', '', '', 'a'],
+            ['a', '', '', '', '', 'b', 'b', '', '', 'a'],
+            ['a', '', '', '', '', '', '', '', '', 'a'],
+            ['a', '', '', '', '', '', '', '', '', 'a'],
+            ['a', 'a', 'a', 'a', 'c', 'c', 'a', 'a', 'a', 'a']
+        ],
+        keys: {
+            'a': new Tile({}), // Need empty object
+            'b': new Tile({passability: 'one-way'}),
+            'c': new Tile({damaging: true})
+        }
+    };
+})());
